f18aa3b4602c83b3cd82ebce626b9451c7641291
sgash708 <sugash7081023@gmail.com>
2021-12-11 14:15:00 +0900

    htmlの埋め込みは使いづらい

--- lesson1/chat/main.go
+++ lesson1/chat/main.go
@@ -7,15 +7,17 @@ import (
 
 // main メイン関数
 func main() {
+	// HTTPリクエスト
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`
-		<html>
-			<head><title>チャット</title></head>
-			<body>チャットしましょう！</body>
-		</html>
-		`))
+<html>
+	<head><title>チャット</title></head>
+	<body>チャットしましょう！</body>
+</html>
+`))
 	})
 
+	// WEBサーバの開始(port: 8080)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("ListenServer: %v", err)
 	}
