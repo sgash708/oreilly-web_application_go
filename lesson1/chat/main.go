7056fe486e362a40ff9d0ff87cf6626016e2778a
sgash708 <sugash7081023@gmail.com>
2021-12-11 14:17:47 +0900

    注意点記述

--- lesson1/chat/main.go
+++ lesson1/chat/main.go
@@ -8,6 +8,11 @@ import (
 // main メイン関数
 func main() {
 	// HTTPリクエスト
+	/*
+		[WARN]
+		"/"以下のリクエストにもマッチする。例) /favicon.ico
+		ブラウザからアクセスすると、2回アクセスされたように見える
+	*/
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`
 <html>
